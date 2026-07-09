51fc8e7f6c2fb521493b78eee939bc5d7e4fc75d
Edgar <davidnavarro81230@outlook.com>
2026-07-08 20:12:42 -0500

    Adding data scroll smooth to html layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -15,7 +15,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en" suppressHydrationWarning>
+    <html lang="en" data-scroll-behavior="smooth" suppressHydrationWarning>
       <body>
         <ThemeProvider>
           {children}
